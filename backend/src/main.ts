4b54688114c0840040b584c9254b2bd87694664f
PizzaPapst <maik.bartels2@gmail.com>
2026-02-19 20:12:12 +0100

    feat: initialize NestJS application with global API prefix and CORS enabled.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api'); // Alle Routen starten jetzt mit /api
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
